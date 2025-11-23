cc0a930be7f0cf72df8def86e53c7788ded29829
Sacha Hibon <sachaedgar@yahoo.fr>
2025-11-23 14:15:17 +0100

    chore: update init.sh so that it setup also the virtual env and the keys

--- init.sh
+++ init.sh
@@ -1,2 +1,9 @@
+echo "SETUP 'PYTHONPATH'"
 export PYTHONPATH="${PYTHONPATH}:${PWD}"
 export PYTHONPATH="${PYTHONPATH}:${PWD}/src"
+
+echo "SETUP KEYS"
+source ./keys.sh
+
+echo "SETUP virtual env"
+source .venv/bin/activate
